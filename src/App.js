e1fc7a6cd332db2878a5a00dce955bb2ed6d6eea
Gustavo Karnopp <gkarnopp794@gmail.com>
2020-04-19 13:39:05 -0300

    Adicionar e Remover intens

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title:'Gustavo Karnopp',
+      url:'https://github.com/GustavoKarnopp',
+      techs: ['Node.js','ReactJS']
+    })
+    setRepositories([ ...respositories, response.data ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filtrer(
+      repository => repository.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => (
+          <li key = {repository.id}>
+            {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
